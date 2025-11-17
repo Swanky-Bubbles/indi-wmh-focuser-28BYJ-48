28a85f5d2676a8789965be65d34ff7113a843160
Danny Kramer <dkramer74@gmail.com>
2025-11-17 14:26:01 -0800

    Refactor CMakeLists.txt and update Waveshare28BYJ class for improved property management and GPIO handling

--- indi_waveshare_28byj48.cpp
+++ indi_waveshare_28byj48.cpp
@@ -1,48 +1,83 @@
 #include "indi_waveshare_28byj48.h"
+#include <unistd.h>
+#include <chrono>
+#include <thread>
 
-Waveshare28BYJ::Waveshare28BYJ() {}
+Waveshare28BYJ::Waveshare28BYJ() : gpio_handle(-1) {}
+
 Waveshare28BYJ::~Waveshare28BYJ() { disconnectHook(); }
 
-bool Waveshare28BYJ::InitProperties()
+bool Waveshare28BYJ::initProperties()
 {
-    // Initialize your switches/numbers here
+    // Position property
+    defineNumber(&position_prop, "POSITION", "Position", current_position, 0, 10000);
+
+    // Direction property
+    defineSwitch(&direction_prop, "DIRECTION", "Direction", {"Forward", "Backward"}, 1);
+
+    // Abort property
+    defineSwitch(&abort_prop, "ABORT", "Abort", {"Abort"}, 0);
+
+    // Microstep property
+    defineSwitch(&microstep_prop, "MICROSTEP", "Microstep", {"1", "2", "4", "8"}, 1);
+
     return true;
 }
 
-bool Waveshare28BYJ::UpdateProperties()
+bool Waveshare28BYJ::updateProperties()
 {
-    // Update properties for KStars
+    if (isConnected())
+    {
+        defineNumber(position_prop);
+        defineSwitch(direction_prop);
+        defineSwitch(abort_prop);
+        defineSwitch(microstep_prop);
+    }
+    else
+    {
+        deleteProperty(position_prop);
+        deleteProperty(direction_prop);
+        deleteProperty(abort_prop);
+        deleteProperty(microstep_prop);
+    }
     return true;
-}
-
-void Waveshare28BYJ::ISNewSwitch(const char *name, const INDI::SwitchVector *svp)
-{
-    // Handle switch events
-}
-
-void Waveshare28BYJ::ISNewNumber(const char *name, const INDI::NumberVector *nvp)
-{
-    // Handle number events
 }
 
 bool Waveshare28BYJ::connectHook()
 {
-    // Initialize GPIO and motor
+    gpio_handle = lgGpiochipOpen(0);
+    if (gpio_handle < 0)
+    {
+        write_status("Failed to open GPIO chip");
+        return false;
+    }
+    write_status("Connected to Waveshare 28BYJ");
     return true;
 }
 
 bool Waveshare28BYJ::disconnectHook()
 {
-    // Cleanup GPIO
+    if (gpio_handle >= 0)
+    {
+        lgGpiochipClose(gpio_handle);
+        gpio_handle = -1;
+    }
+    write_status("Disconnected");
     return true;
 }
 
-void Waveshare28BYJ::move_steps(long long steps)
+void Waveshare28BYJ::ISNewSwitch(const char *dev, const char *name, const IUSwitchVectorProperty *svp)
 {
-    // Motor stepping code using lgpio
-}
-
-void Waveshare28BYJ::write_status(const string &s)
-{
-    // Write status messages
-}
+    if (IUFindSwitch(svp, "DIRECTION"))
+    {
+        // read direction
+    }
+    else if (IUFindSwitch(svp, "ABORT"))
+    {
+        // handle abort
+    }
+    else if (IUFindSwitch(svp, "MICROSTEP"))
+    {
+        // set microstepping
+    }
+    IDSetSwitch
--- indi_waveshare_28byj48.h
+++ indi_waveshare_28byj48.h
@@ -1,35 +1,41 @@
-#pragma once
+#ifndef INDIDEV_WAVESHARE_28BYJ48_H
+#define INDIDEV_WAVESHARE_28BYJ48_H
 
-#include <indidevapi.h>
-#include <indicom.h>
 #include <indifocuser.h>
-#include <indibase.h>
 #include <lgpio.h>
 #include <string>
 
 using namespace std;
 
-class Waveshare28BYJ : public INDI::DefaultDevice
+class Waveshare28BYJ : public INDI::Focuser
 {
 public:
     Waveshare28BYJ();
     ~Waveshare28BYJ();
 
-    bool InitProperties() override;
-    bool UpdateProperties() override;
+    bool initProperties() override;
+    bool updateProperties() override;
+
     const char *getDefaultName() override { return "Waveshare_28BYJ"; }
 
-    void ISNewSwitch(const char *name, const INDI::SwitchVector *svp) override;
-    void ISNewNumber(const char *name, const INDI::NumberVector *nvp) override;
+    void ISNewSwitch(const char *dev, const char *name, const IUSwitchVectorProperty *svp) override;
+    void ISNewNumber(const char *dev, const char *name, const IUNumberVectorProperty *nvp) override;
 
     bool connectHook() override;
     bool disconnectHook() override;
 
 private:
-    ISwitchVectorProperty *dir_prop = nullptr;
-    ISwitchVectorProperty *abort_prop = nullptr;
-    ISwitchVectorProperty *ms_prop = nullptr;
+    int gpio_handle;
+    long long current_position = 0;
+    long long target_position = 0;
+
+    IUSwitchVectorProperty *direction_prop = nullptr;
+    IUSwitchVectorProperty *abort_prop = nullptr;
+    IUSwitchVectorProperty *microstep_prop = nullptr;
+    IUNumberVectorProperty *position_prop = nullptr;
 
     void move_steps(long long steps);
     void write_status(const string &s);
 };
+
+#endif
